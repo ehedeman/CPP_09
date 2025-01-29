ded0e4171924cb051cb4a15a25759ea316e81f79
Emma Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2025-01-29 14:35:52 +0100

    idk

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 14:31:33 by ehedeman          #+#    #+#             */
+/*   Updated: 2025/01/29 14:00:34 by ehedeman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <cstring>
+#include <string>
+ 
