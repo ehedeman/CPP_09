2fab07a76a50d064694bb5f54bdbd5895a90fa84
Marlon Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2025-06-26 13:57:08 +0200

    added commentary to ex02, fixed some stuff regarding one number as input

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 12:00:33 by ehedeman          #+#    #+#             */
-/*   Updated: 2025/06/17 13:13:43 by ehedeman         ###   ########.fr       */
+/*   Updated: 2025/06/26 13:55:17 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,8 +116,8 @@ void			PmergeMe::readArgs()
 		checkDouble(_new);
 		this->_s.push_back(_new);
 	}
-	if (_s.size() < 2)
-		return ;
+	// if (_s.size() < 2)
+	// 	return ;
 	std::vector<unsigned int>::iterator it = _s.begin();
 	while (it != _s.end())
 	{
@@ -203,7 +203,12 @@ template <typename T> void	PmergeMe::printContainer(T &array, \
 void	PmergeMe::mergeInsertionSort(std::vector<unsigned int> &array, \
 	int sorting_level)
 {
-	typedef std::vector<unsigned int>::iterator Iterator;
+	if (array.size() < 2)
+	{
+		gettimeofday(&t_vec_end, NULL);	
+		return ;
+	}
+		typedef std::vector<unsigned int>::iterator Iterator;
 
 	int pair_amount = array.size() / sorting_level;
 	if (pair_amount < 2)
@@ -229,20 +234,9 @@ void	PmergeMe::mergeInsertionSort(std::vector<unsigned int> &array, \
 			swapPairs(_this, sorting_level);
 		}
 	}
-
-	// std::vector<unsigned int>::iterator print_iterator = array.begin();
-	// std::cout << "SORTING_LEVEL: " << sorting_level << " || ";
-	// while (print_iterator != array.end())
-	// {
-	// 	std::cout << *print_iterator << " ";
-	// 	print_iterator++;
-	// }
-	
-	std::cout << std::endl << std::endl;
 	
 	
 	mergeInsertionSort(array, steps);
-
 
 	std::vector<Iterator> main;
 	std::vector<Iterator> to_append;
@@ -261,13 +255,17 @@ void		PmergeMe::insert(std::vector<unsigned int> &array, \
 
 	main.insert(main.end(), next_iterator(array.begin(), sorting_level - 1));
 	main.insert(main.end(), next_iterator(array.begin(), sorting_level * 2 - 1));
-	
+	//takes the first two pairs
 	for (int i = 4; i <= pair_amount; i += 2)
 	{
 		to_append.insert(to_append.end(), next_iterator(array.begin(), \
 			sorting_level * (i - 1) - 1));
 		main.insert(main.end(), next_iterator(array.begin(), sorting_level * i - 1));
 	}
+
+	//!!!main at this point is already sorted
+	// to_append isnt
+	
 
 	if (is_odd)
     {
@@ -322,32 +320,42 @@ void	PmergeMe::jacobsthal_number(std::vector<std::vector<unsigned int>::iterator
 {
 	typedef std::vector<unsigned int>::iterator Iterator;
 
-	int prev_j_nbr = _jacobsthal_nbr(1);
-	int	_amount_inserts = 0;
-	for (int k = 2;; k++)
+	int prev_j_nbr = _jacobsthal_nbr(1);	//gotta start somewhere so jn(1) as first previous
+	int	_amount_inserts = 0;	//makes sense
+	for (int k = 2;; k++)	//k = 2 because jn(1) is already the previous and k gets used as jn(k)
 	{
-		int _current_j_nbr = _jacobsthal_nbr(k);
-		int _j_difference = _current_j_nbr - prev_j_nbr;
-		int _offset = 0;
-		if (_j_difference > static_cast<int>(to_append.size()))
+		int _current_j_nbr = _jacobsthal_nbr(k);	//get the jn
+		int _j_difference = _current_j_nbr - prev_j_nbr; //get the difference of currnet and prev (that will be the amount of inserted objects)
+		int _offset = 0;	//offset = 0 cause new iteration of loop
+		if (_j_difference > static_cast<int>(to_append.size())) // if the difference is larger than array (makes sense)
 			break ;
-		int _counter = _j_difference;
+		int _counter = _j_difference;	//we iterate counter times so as many times as the difference is large	
 		std::vector<Iterator>::iterator pendIt = \
-			next_iterator(to_append.begin(), _j_difference - 1);
+			next_iterator(to_append.begin(), _j_difference - 1);	//the iterator of pend gets set to the differnece -1
 		std::vector<Iterator>::iterator bountIt = \
-			next_iterator(main.begin(), _current_j_nbr + _amount_inserts);
-		while (_counter)
+			next_iterator(main.begin(), _current_j_nbr + _amount_inserts);//iterator of bound gets set to curr jn + the amount of inserts we had so far
+		
+		// the jn helps finding the range of numbers to insert in one iteration.
+		// that way there is an order in which to insert, which saves us comparisons
+		// if we have e.g 1b 1a 2a as objects and we want to insert 3b, its much faster
+		// than if we take the whole of 1b 1a 2a 3a 5a 2a 6a 7a because instead of comparing
+		// 3b to all a objects in main we just need to compare it to the range we know it belongs
+		// into. because of the nature of this algorithm we know that 3b is most likely larger than
+		// 2a, but anything after 3a is bigger anyways.
+	
+		while (_counter)	//binary search insertion parrrttt
 		{
 			std::vector<Iterator>::iterator idx = \
 				std::upper_bound(main.begin(), bountIt, *pendIt,\
-					 _operator<Iterator>);
+					 _operator<Iterator>);	// upper_bound is a function that uses binary search
 			std::vector<Iterator>::iterator inserted = \
-				main.insert(idx, *pendIt);
+				main.insert(idx, *pendIt);	//so binary search is being used to find where to insert
 			_counter--;
 			pendIt = to_append.erase(pendIt);
 			std::advance(pendIt, -1);
 	
 			_offset += (inserted - main.begin() == _current_j_nbr + _amount_inserts);
+			// offset is nessecary due to rounding errors
 			bountIt = next_iterator(main.begin(), _current_j_nbr + _amount_inserts - _offset);
 		}
 		prev_j_nbr = _current_j_nbr;
@@ -361,6 +369,11 @@ void	PmergeMe::jacobsthal_number(std::vector<std::vector<unsigned int>::iterator
 void		PmergeMe::mergeInsertionSort(std::deque<unsigned int> &array, \
 	int sorting_level)
 {
+	if (array.size() < 2)
+	{
+		gettimeofday(&t_deq_end, NULL);	
+		return ;
+	}
 	typedef std::deque<unsigned int>::iterator Iterator;
 
 	int pair_amount = array.size() / sorting_level;
@@ -390,6 +403,9 @@ void		PmergeMe::mergeInsertionSort(std::deque<unsigned int> &array, \
 	std::deque<Iterator> to_append;
 
 	insert(array, main, to_append, sorting_level, pair_amount, is_odd, end);
+	//copying all objects from main to our original so it gets updated for all recursions
+	// recursion waits for the "child" to finish. so we can safely copy without having to worry about
+	// messing up data for the other recursion steps
 	_copy(array, sorting_level, main);
 	gettimeofday(&t_deq_end, NULL);
 }
@@ -427,6 +443,8 @@ void		PmergeMe::insert(std::deque<unsigned int> &array, std::deque<std::deque<un
 			std::upper_bound(main.begin(), _current_bound, *_current_append, _operator<Iterator>);
 		main.insert(idx, *_current_append);
 	}
+	//if there are no jn left, but still objects in to_append then we insert them here,
+	//starting from the back and in reverse order to still minimise the amount of inserts.
 }
 
 void PmergeMe::_copy(std::deque<unsigned int> &array, int sorting_level, \
